adefa10192dddfd4c3cd0acf2ed5e2e32a0bb6c8
unknown <shohan.cnits@gmail.com>
2024-03-24 12:32:01 -0700

    slider done

--- assets/js/hero-slider.js
+++ assets/js/hero-slider.js
@@ -1,7 +1,12 @@
 var swiper = new Swiper(".mySwiper", {
     direction: "vertical",
     slidesPerView: 1,
-    spaceBetween: 30,
+    spaceBetween: 0,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop: true,
     mousewheel: true,
     pagination: {
       el: ".swiper-pagination",
